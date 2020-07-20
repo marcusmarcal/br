c6c678993ede9bf6aa0e6546082b4a81ede5af46
Marcus Marçal <marcus.marcal@gmal.com>
2020-07-20 11:50:41 -0300

    adicionado "php"

--- add.php
+++ add.php
@@ -1,4 +1,4 @@
-<?
+<?php
 include("conexao.php");
 if ($numero_nota != ""){
 	$numero_nota = $numero_nota; }
@@ -8,16 +8,16 @@ $data_nota = converte_data("$data_nota");
 $add = "insert into $tabela
 (nome_fornecedor,natureza,numero_nota,data_nota,transportadora,ordem_compra,data_recebimento,autorizacao,responsavel,obs) values
 (\"$nome_fornecedor\",\"$natureza\",\"$numero_nota\",\"$data_nota\",\"$transportadora\",\"$ordem_compra\",\"$data_recebimento\",\"$autorizacao\",\"$responsavel\",\"$obs\")";
-mysql_query($add,$conexao) or die("<font color=\"red\">Erro: Não foi possível gravar dados no banco!</font><br><br><a href=\"novo.php\"><font color=\"#003399\">Voltar</font></a>");
+mysql_query($add,$conexao) or die("<font color=\"red\">Erro: Nï¿½o foi possï¿½vel gravar dados no banco!</font><br><br><a href=\"novo.php\"><font color=\"#003399\">Voltar</font></a>");
 
 $consulta = "select * from $tabela";
-$resultado = mysql_query($consulta,$conexao) or die("<font color=\"#003399\">Não foi possivel conectar ao banco da dados");
+$resultado = mysql_query($consulta,$conexao) or die("<font color=\"#003399\">Nï¿½o foi possivel conectar ao banco da dados");
 
 while ($linha = mysql_fetch_row($resultado)) {
 $numero_br = $linha[0];
 }
 ?>
-<h2><font color="#003399">Adicionar produtos no BR número <? echo $numero_br; ?>!</font></h2>
+<h2><font color="#003399">Adicionar produtos no BR nï¿½mero <? echo $numero_br; ?>!</font></h2>
 <form method="POST" action="add_produtos.php">
 <input type="hidden" name="numero_br" value="<? echo $numero_br; ?>">
   <table border="0" width="100%" style="font-family: Arial; color: #697BFC">
@@ -29,7 +29,7 @@ include("produtos.inc");
 ?>
 </td></tr>
     <tr>
-      <td width="19%">Número de série:</td>
+      <td width="19%">Nï¿½mero de sï¿½rie:</td>
       <td width="81%"><input type="text" name="numero_serie" size="35" style="font-family: Arial; color: #FFFFFF; background-color: #C0C0C0"></td>
     </tr>
     <tr>
