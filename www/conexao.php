d7d86040a32f2833309613465a51fa13652c4b17
Marcus Marçal <marcus.marcal@gmail.com>
2020-09-16 21:15:57 -0300

    Tentando conectar ap banco

--- www/conexao.php
+++ www/conexao.php
@@ -2,7 +2,7 @@
 require "topo.php";
 require "funcoes.php";
 
-$bdServidor = '127.0.0.1';
+/*$bdServidor = '127.0.0.1';
 $bdUsuario = 'root';
 $bdSenha = 'Uws012aQ=!';
 $bdBanco = 'br';
@@ -11,8 +11,21 @@ if (mysqli_connect_errno($conexao)) {
     echo "Problemas para conectar no banco. Verifique os dados!" . PHP_EOL;
     die();
 }
+*/
 
 $tabela = "index_br";
+
+$host = “db”;
+$username = “root”;
+$password = 'Uws012aQ=!';
+$db = “br”;
+try {
+$conexao = new PDO("mysql:host=$host;dbname=$db", $username, $password);
+$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo “<h2>Conectado com sucesso.<h2>”;
+} catch(PDOException $e) {
+echo ‘ERROR: ‘ . $e->getMessage();
+}
 
 //echo "Conexão: " . gettype($conexao) . PHP_EOL;
 //echo "Select: " . gettype($select) . PHP_EOL;
